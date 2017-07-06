d19fe280f781a90b3fc2bec06fc351f036c0378e
skropf <simon.kropf@gmail.com>
2017-07-06 12:13:46 +0200

    adding sources

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject evolved-joshua "0.1.0-alpha"
+  :description "Conversational bot which uses twitter as interface and Cleverbot as answering AI"
+  :url "https://github.com/skropf/evolved-joshua"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [proto-repl "0.3.1"]
+                 [clj-http "3.6.1"]
+                 [environ "1.0.0"]
+                 [overtone/at-at "1.2.0"]
+                 [twitter-api "0.7.8"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.slf4j/slf4j-log4j12 "1.7.12"]
+                 [log4j/log4j "1.2.17"]]
+   :main evolved-joshua.core
+   :min-lein-version "2.0.0"
+   :plugins [[lein-environ "1.0.0"]])
